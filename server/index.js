e749d3a85b3b03128a87a8b3ba64cf8523a97aea
raulsync <rahul.aanand086@gmail.com>
2024-10-19 03:51:49 +0530

    cors-err-fix

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+const router = require("./routes");
 
 const app = express();
 
-const port = 5000;
-
-app.get("/hello", (req, res) => {
-  res.status(200).json({ mssg: "Hello World" });
-});
+const port = 8888;
+app.use(cors());
+app.use(express.json());
+app.use("/api", router);
 
 app.listen(port, () => {
   console.log(`server is listening on http://localhost:${port}`);
