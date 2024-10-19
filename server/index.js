0f5140fff23f26fbfd347685b17c2433963ca158
raulsync <rahul.aanand086@gmail.com>
2024-10-19 06:26:34 +0530

    db_backend_connection

--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const router = require("./routes");
 
+const { connectToMongoDb } = require("./dataBase");
+
 const app = express();
 
-const port = 8888;
+const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 
-app.listen(port, () => {
-  console.log(`server is listening on http://localhost:${port}`);
-});
+async function startServer() {
+  await connectToMongoDb();
+  console.log("MongoDB Connected");
+
+  app.listen(port, () => {
+    console.log(`server is listening on http://localhost:${port}`);
+  });
+}
+
+startServer();
